2bddc50a6fdc8fa5ee96ba4ff55bed6776194066
Tanya Saroha <tanya@joshsoftware.com>
2020-01-31 23:15:27 +0530

    Controller and View for Adding new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,22 @@ class UsersController < ApplicationController
   def index
     @users = User.all.order(:full_name)
   end
+
+  def new
+    @user = User.new
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def create
+    user = User.create(permit_params.merge(status: 'active'))
+    redirect_to users_path and return if !user.errors.present?
+  end
+
+  private
+
+  def permit_params
+    params.require(:user).permit(:full_name, :mobile_number, :email, :role)
+  end
 end
