12a2b7ca7bebbf17a5d5c0393e714b1bb052a202
Tanya Saroha <tanya@joshsoftware.com>
2020-01-31 19:45:45 +0530

    Added validations on User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :full_name, :email, :role, presence: true
+  validates :email, format: { with: EMAIL_REGEXP, message: "Email must be in format tanya@example.com"} 
 end
